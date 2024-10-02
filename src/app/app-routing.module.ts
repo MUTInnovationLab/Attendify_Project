644d5631b5773e0d2089e4a250c1206510ae5c23
Nkazi01 <nkazimulomongezi72@gmail.com>
2024-10-02 14:47:17 +0200

    timeTable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -95,7 +95,8 @@ const routes: Routes = [
   {
     path: 'full-calendar',
     loadChildren: () => import('./full-calendar/full-calendar.module').then( m => m.FullCalendarPageModule)
-  },  {
+  },
+  {
     path: 'dept-analytics',
     loadChildren: () => import('./dept-analytics/dept-analytics.module').then( m => m.DeptAnalyticsPageModule)
   },
