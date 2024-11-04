008e09bff9071cbc7eee435885e73a2ea56067e5
HlehleRH <hlehlerhamahle@gmail.com>
2024-11-04 15:36:14 +0200

    changes

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -255,7 +255,7 @@ export class DashboardPage {
 
   nabigateToAddModule() {
     this.navCtrl.navigateForward('/faculty-form'); 
-    this.navCtrl.navigateForward('/admin'); 
+    
   }
 
   departmentsAnalytics = [
